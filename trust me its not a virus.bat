cb83f12c8698f022d974c8f40101ee43c63f0b68
at1123 <ahnaftahsin99@gmail.com>
2024-12-26 22:19:09 +0600

    Add files via upload

---
+++ trust me its not a virus.bat
@@ -0,0 +1,6 @@
+@echo off
+for /l %%a in (1,1,50) do (
+    mkdir Folder_%%a
+)
+echo If you trust any programmer like AHNAF TAHSIN, congratulations! You've just summoned 50 random folders and maybe a secret folder named 'Oops, My Bad'!
+pause
