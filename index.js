d9ed1b6f6005fea9546243bc935ee72ae9736e6f
at1123 <ahnaftahsin99@gmail.com>
2025-03-12 10:53:25 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function login() {
     // Check credentials
     if (username === correctUsername && password === correctPassword) {
         // Redirect to another page if credentials are correct
-        window.location.href = 'index1.html';
+        window.location.href = 'i am a programmer.html';
     } else {
         // Show error message if credentials are incorrect
         document.getElementById('error-message').textContent = 'Invalid username or password. Please try again.';
